5521a3515272ee5d23e7d6e6de7bbb3ad699ce2d
Ricardo F. Hernandez <ricardo.nfe.homine@gmail.com>
2023-06-03 15:15:58 -0300

    progress to build the OSD2 page configure the animation script in all pages

--- js/script.js
+++ js/script.js
@@ -1,30 +1,3 @@
-// 1. Efeito de descriptografia
-const letters = 'abcdefghijklmnopqrstuvwxyz';
-
-const decode = document.querySelector('#decode');
-
-function hackerNumber() {
-  let iterations = 0;
-
-  const interval = setInterval(() => {
-    decode.innerText = decode.innerText
-      .split('')
-      .map((_, index) => {
-        if (index < iterations) {
-          return decode.dataset.value[index];
-        }
-        return letters[Math.floor(Math.random() * 26)];
-      })
-      .join('');
-
-    if (iterations >= decode.dataset.value.length) {
-      clearInterval(interval);
-    }
-
-    iterations += 1 / 3;
-  }, 30);
-}
-hackerNumber();
 
 // 2. Script para selecionar texto dinamicamente de acordo com a seleção do carrosel
 let activeIndex = 0;
